ab883a287a43d98dd85f298a5fd9c9e268306292
Souvik Kundu <kivtech88@gmail.com>
2024-08-25 12:29:00 +0530

    commit

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!\n");
+   return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(void) {
+   printf("Hello, World!\n");
+   return 0;
+}
---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int age = 30;
+    float pi = 3.14159;
+    char initial = 'J';
+
+    printf("Hello, %c!\n", initial);
+    printf("You are %d years old.\n", age);
+    printf("The value of pi is approximately %.2f\n", pi);
+
+
+
+    return 0;
+}
