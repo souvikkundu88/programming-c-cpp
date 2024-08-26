90e61442ca28130d709ba0230efbc004a1205f7f
Souvik Kundu <kivtech88@gmail.com>
2024-08-26 17:44:26 +0530

    commit

--- votingAge.c
+++ votingAge.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 
+int main(void) {
+    int age;
+
+    printf ("Enter your age: ");
+    scanf ("%d", &age);
+
+    if (age >= 18) {
+        printf("Your and your are eligible to vote\n");
+    } else {
+        printf(" You are not eligible to vote");
+    }
+
+    return 0;
+}
