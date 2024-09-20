94cec2c10157e5984088a32ac7171cb35f18fd53
Souvik Kundu <kivtech88@gmail.com>
2024-09-20 17:22:37 +0530

    commit

--- codeSnippets.c
+++ codeSnippets.c
@@ -1,12 +1,22 @@
+/******************************************/
 // input arguments
-
 #include <stdio.h>
+// void writeMessage(char name[]) {
+//     printf("Hello, %s\n", name);
+// }
+// int main(void) {
+//     writeMessage("Dave");
+//     writeMessage("Victoria");
+// }
 
-void writeMessage(char name[]) {
-    printf("Hello, %s\n", name);
+void writeMessage(char name[], int age) {
+    printf("hello, you are %s, and your age is %d\n", name, age);
 }
 
 int main(void) {
-    writeMessage("Dave");
-    writeMessage("Victoria");
+    writeMessage("Dave", 34);
+    writeMessage("SK", 36);
+    return 0;
 }
+
+/******************************************/
