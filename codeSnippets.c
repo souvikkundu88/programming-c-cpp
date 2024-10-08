7d5874c5de35fa44060dabae31ede7304cfcf81d
Souvik Kundu <kivtech88@gmail.com>
2024-10-08 14:08:55 +0530

    Update codeSnippets.c

--- codeSnippets.c
+++ codeSnippets.c
@@ -345,3 +345,34 @@ Use if else
 // }
 
 // optimizing the previous
+
+#include <stdio.h>
+
+int is_even(int userInput) {
+  if (userInput == 0) {
+    printf("%d is zero\n", userInput);
+    return 1; 
+  }
+
+  if (userInput % 2 == 0) { 
+    printf("%d is even and ", userInput);
+  } else {
+    printf("%d is odd and ", userInput);
+  }
+
+  if (userInput > 0) {
+    printf("a positive integer\n");
+  } else {
+    printf("a negative integer\n");
+  }
+  // Simplified even/odd check:
+  return (userInput % 2 == 0); 
+}
+
+int main() {
+  int num;
+  printf("Enter an integer: ");
+  scanf("%d", &num);
+  is_even(num);
+  return 0; // It's good practice to return 0 from main
+}
