3d51a3d929168f58f1603c6a8cf30ea5bb65a291
Souvik Kundu <kivtech88@gmail.com>
2024-10-27 10:56:02 +0530

    commit

--- codeSnippets.c
+++ codeSnippets.c
@@ -377,45 +377,70 @@ Use if else
 //   return 0; // It's good practice to return 0 from main
 // }
 
+// #include <stdio.h>
+
+// void fibonacci(int n) {
+//     // First two numbers of Fibonacci
+//     long long first = 0, second = 1, next;
+    
+//     printf("Fibonacci Series up to %d numbers:\n", n);
+    
+//     // Print the first number if n is at least 1
+//     if (n >= 1)
+//         printf("%lld ", first);
+    
+//     // Print the second number if n is at least 2
+//     if (n >= 2)
+//         printf("%lld ", second);
+
+//     // Generate the rest of the series
+//     for(int i = 3; i <= n; i++) {
+//         next = first + second;
+//         printf("%lld ", next);
+//         first = second;
+//         second = next;
+//     }
+    
+//     printf("\n");
+// }
+
+// int main() {
+//     int numTerms;
+    
+//     // Ask user for number of terms
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &numTerms);
+    
+//     // Ensure the number of terms is positive
+//     if (numTerms <= 0) {
+//         printf("Please enter a positive integer.\n");
+//     } else {
+//         fibonacci(numTerms);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
-void fibonacci(int n) {
-    // First two numbers of Fibonacci
-    long long first = 0, second = 1, next;
-    
-    printf("Fibonacci Series up to %d numbers:\n", n);
-    
-    // Print the first number if n is at least 1
-    if (n >= 1)
-        printf("%lld ", first);
-    
-    // Print the second number if n is at least 2
-    if (n >= 2)
-        printf("%lld ", second);
-
-    // Generate the rest of the series
-    for(int i = 3; i <= n; i++) {
-        next = first + second;
-        printf("%lld ", next);
-        first = second;
-        second = next;
-    }
-    
-    printf("\n");
+int Fibonacci(int n) {
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-int main() {
-    int numTerms;
-    
-    // Ask user for number of terms
-    printf("Enter the number of terms: ");
-    scanf("%d", &numTerms);
-    
-    // Ensure the number of terms is positive
-    if (numTerms <= 0) {
-        printf("Please enter a positive integer.\n");
-    } else {
-        fibonacci(numTerms);
+// int main(void) {
+//     int n = 10;
+//     int Fn = Fibonacci(n);
+//     printf("Fibonacci(%d) = %d\n", n, Fn);
+//     return 0;
+// }
+
+int main(void) {
+    int n = 10;
+    printf("Fibonacci series up to %d terms: ", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", Fibonacci(i));
     }
+    printf("\n");
     return 0;
 }
