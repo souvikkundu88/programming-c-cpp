7fae108d696e7487836a94cdea99fc8a1ca783d7
Souvik Kundu <kivtech88@gmail.com>
2025-10-21 18:05:31 +0530

    Update codeSnippets.c

--- codeSnippets.c
+++ codeSnippets.c
@@ -736,7 +736,7 @@ int main(void) {
 
 //   return 0;
 // }
-
+/*
 #include<stdio.h>
 #include<stdio.h>
 
@@ -747,3 +747,19 @@ int main(void) {
   double myDouble = atof(floatString);
   printf("intString = %s")
 }
+*/
+/*
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("a char is %ld bytes\n", sizeof(char));
+    printf("an int is %ld bytes\n", sizeof(int));
+    printf("an float is %ld bytes\n", sizeof(float));
+    printf("a double is %ld bytes\n", sizeof(double));
+    printf("a short int is %ld bytes\n", sizeof(short int));
+    printf("a long int is %ld bytes\n", sizeof(long int));
+    printf("a long double is %ld bytes\n", sizeof(long double));
+    return 0;
+}
+*/
+
